173058327e7d50ad5ed4eec344be1e220e5a5762
Shubham Rawat <46570973+shubhamrawat140798@users.noreply.github.com>
2022-10-06 00:45:36 +0530

    5 oct 2022

---
+++ 5-oct-2022/EmpNameStartsWithCharacter.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+ 
+class Employee {
+    private String name;
+    private int salary;
+    public Employee(String name, int salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+    public String getName(){
+        return name;
+    }
+    
+    public Integer getSalary(){
+        return salary;
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+      List<Employee> employeeList = new ArrayList<>();
+      employeeList.add(new Employee("Frank",1000));
+      employeeList.add(new Employee("Alex",1800));
+      employeeList.add(new Employee("Simon",1200));
+	employeeList.add(new Employee("Adarsh",2000));
+      employeeList.add(new Employee("Bob",1400));
+      employeeList.add(new Employee("Saksham",3000));
+        employeeList.forEach(e -> System.out.println(e.getName ()+" "+ e.getSalary()));
+        System.out.println("\n---------Employee name Start with S----------");
+         employeeList.stream()
+            .filter(e -> (e.getName()).startsWith("S"))
+            .forEach(e -> System.out.println(e.getName() + " " + e.getSalary()));
+    }
+}
